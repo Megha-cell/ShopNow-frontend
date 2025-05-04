d7089a6c646fb6e3b6956f6697e173e96cc7a443
Megha-cell <meghakamlekar123@gmail.com>
2025-05-04 20:55:36 +0530

    Updated Frontend build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdminDashboard from "./pages/AdminDashboard";
+import AdminDashboard from "./pages/AdminDashboard";//import Amin Dasboard
 import RoleRoute from "./components/RoleRoute";
 import Home from "./pages/Home";
 import About from "./pages/About";
