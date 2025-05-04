c8cc6385bbcadf019a99c73e7df69a275e475bc1
Megha-cell <meghakamlekar123@gmail.com>
2025-05-04 23:22:46 +0530

    last final123

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import '../css/Home.css';
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -6,7 +7,9 @@ function Home() {
       <div className="home-content">
         <h1 className="home-title">Welcome to <span className="brand-name">ShopNow</span></h1>
         <p className="home-subtitle">Find quality products at unbeatable prices.</p>
-        <a href="/products" className="home-btn">Shop Now</a>
+        {/* <a href="/products" className="home-btn">Shop Now</a> */}
+        <Link to="/products" className="home-btn">Shop Now</Link>
+
       </div>
     </div>
   );
